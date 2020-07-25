251bb59cec178c3d981d3ce6536c925aacb0be62
khalidaria0422 <68077139+khalidaria0422@users.noreply.github.com>
2020-07-24 23:39:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,383 @@
+/*
+
+//Variables
+var myPenisSize = '32';
+console.log(myPenisSize);
+
+//Number, String, Boolean, Undefined, Null
+
+//Coercion
+
+if (myPenisSize == 32) {
+	console.log('My penis is this');
+} else {
+	console.log(false);
+}
+
+//Logs
+/*
+var showThis = 'Man khar astum';
+
+console.log(showThis);
+alert(showThis);
+var userInput = prompt('What is your dick size: ');
+
+console.log(userInput);
+*/
+//Math operators
+
+//+-/* 
+//console.log(showThis + ' ' + userInput);
+/*
+var a = 54;
+var b = 32;
+var c = a + b;
+console.log(a);
+var a = 87;
+console.log(a);
+var a = 'ggs in the chat';
+console.log(a);
+var a = null;
+console.log(a);
+var a = undefined;
+console.log(a);
+
+
+//l;kasdjf;laskjflkjf           l;kjasfl;kjsad
+/*
+akjsdfl;ksajf;
+lkksajf;lj
+;las;kfd;lk
+kllsajhf
+*/
+/*
+var a = 5;
+var b = 10;
+var c = 3;
+
+var d = (a + b) / c; // 5
+console.log(d);
+
+var phone = iphone = 'iphone 10x';
+console.log(phone);
+
+console.log(a, b, c);
+*/
+
+//typeof
+/*
+var a = 1;
+var b = 2.5;
+var c = 'alsjkdhf';
+var d = true;
+var e;
+var f = null;
+
+console.log(typeof(f));
+
+console.log('Moheb\'s asldjkflasjkf'); // moheb's
+
+var c = a < b;
+console.log(c);
+*/
+
+//nameWaleed + '\'s dick size is: ' + sizeWaleed
+
+
+
+
+//CHALLENGE NUMBER 1
+
+/*
+Waleed and Fakher are trying to compare their dick size. Dick size is calculated from getting the dick height and dick girth.
+
+1.  Store waleed's and Fakher's name in two inputs
+2.  calculate dick size (by multiplying dick (height and dick girth) - 1 just because)
+3.  store both dick sizes in two variables and calculate whos dick size is bigger than the other.
+4.  output whos the winner and their dick size.
+*/
+
+/*
+var userInputName1 = 'Waleed';
+var userInputName2 = 'Fakher';
+
+
+var dickHeight1 = prompt("What's Your Dick Height, " + userInputName1 + "?");
+var dickGirth1 = prompt("What's Your DIck Girth, " + userInputName1 + "?");
+
+var dickHeight2 = prompt("What's Your Dick Height, " + userInputName2 + "?" );
+var dickGirth2 = prompt("What's Your Dick Girth, " + userInputName2 + "?");
+
+var dickSize1 = dickHeight1 * dickGirth1 - 1;
+var dickSize2 = dickHeight2 * dickGirth2 - 1;
+
+var dickSize = dickSize1 > dickSize2;
+
+console.log("Is Waleed's dick bigger than Fakher?: " + dickSize);
+console.log("Waleed's Dick Size is " + dickSize1 + "\'\'");
+console.log("Fakher's Dick Size is " + dickSize2 + "\'\'");
+*/
+
+//boolean operators
+
+// (((&& || !)))
+/*
+
+//if else statements
+var name = prompt('What is your name: ');
+
+if (name === 'Waleed' || name === 'waleed' || name === 'WALEED') {
+	console.log('Cock size is 5 inches.');
+} else if (name === 'Fakher' || name === 'fakher' || name === 'FAKHER') {
+	console.log('Cock size is 3 inches.');
+} else {
+	console.log('Your cock is irrelevant.');
+};
+*/
+/*
+var name = 'Fakher';
+var age = 25;
+
+if (name === 'Waleed' && age === 21) {
+	console.log('Hey ' + name + ' you are too young, drink titty milk.');
+} else if (name === 'Fakher' && age === 25) {
+	console.log('Hey ' + name + ' you are old enough, you can drink my cum.');
+} else {
+	console.log('You are not allowed in.');
+};
+*/
+/*
+var oldEnough = 18;
+var waleedAge = prompt('What is your age: ');
+var fakherAge = prompt('What is your age: ');
+
+if (fakherAge >= oldEnough && waleedAge >= oldEnough) {
+	console.log('Welcome spandiyaa.');
+} else {
+	console.log('Baraayen kharkusaa, club ba shumaa nest.');
+};
+*/
+
+//Calculator
+/*
+console.log('Calculator');
+var userMenuInput = prompt('For multiplication type 1, for division type 2, for addition type 3, for subtracion type 4: ');
+userMenuInput = Number(userMenuInput);
+
+if (userMenuInput === 1) {
+	var userFirstInput = prompt('Write a number to multiply: ');
+	var userSecondInput = prompt('Write another number to multiply the previous number to: ');
+	var calc = userFirstInput * userSecondInput;
+	console.log(calc);
+} else if (userMenuInput === 2) {
+	var userFirstInput = prompt('Write a number to divide: ');
+	var userSecondInput = prompt('Write another number to divide the previous number to: ');
+	if (typeof(userSecondInput) !== undefined) {
+		var calc = userFirstInput / userSecondInput;
+		console.log(calc);
+	} else {
+		console.log('Undefined.');
+	};
+	
+} else if (userMenuInput === 3) {
+	var userFirstInput = prompt('Write a number for addition: ');
+	var userSecondInput = prompt('Write another number to add to the previous number: ');
+	var calc = Number(userFirstInput) + Number(userSecondInput);
+	console.log(calc);
+} else if (userMenuInput === 4) {
+	var userFirstInput = prompt('Write a number to subtract: ');
+	var userSecondInput = prompt('write another number to subtract to the previous number: ');
+	var calc = userFirstInput - userSecondInput;
+	console.log(calc);
+} else {
+	console.log('Invalid Number');
+};
+*/
+
+//var age = 16;
+/*
+if (age < 16) {
+	console.log('Waleed is shit.');
+} else {
+	console.log('Waleed sucks Moheb\'s cock.');
+};
+*/
+//age === 16 ? console.log('Waleed is shit.') : console.log('Waleed sucks moheb\'s cock.');
+/*
+var candyInput = prompt('What color is your candy?: ');
+
+switch(candyInput) {
+	case 'green':
+	console.log('Green is a shit color.');
+	break;
+	case 'blue':
+	console.log('Blue, just like the color of the ocean.');
+	break;
+	case 'red':
+	console.log('Red is the color of my cock.');
+	break;
+	default:
+	console.log('Invalid input.');
+};
+
+
+//falsy values: undefined, null, 0, NaN
+var g;
+
+if(g) {
+	console.log('Truthy');
+}else {
+	console.log('Falsy');
+};
+*/
+/*
+function calculateAreaOfTriangle (base, height) {
+	var areaTriangle = (.5) * base * height;
+	console.log(areaTriangle);
+};
+
+var userInput = prompt('First input: ');
+var userSecondInput = prompt('Second input: ');
+*/
+
+/*
+if (typeof(userInput) === String || typeof(userSecondInput) === String) {
+	console.log('Can\'t do that');
+} else {
+	calculateAreaOfTriangle(userInput, userSecondInput);
+};
+*/
+/*
+//Function Declaration
+function validateAge (age, legalDrinkingAge, name) {
+	if (age < legalDrinkingAge) {
+		console.log('Zane ' + name + ' is not old enough.');
+	} else {
+		console.log('Zane ' + name + ' can drink a champagne along with my cum.');
+	};
+};
+
+const legalDrinkingAge = 21;
+var name = prompt('What is your name?: ');
+var age = prompt('What is zane '+ name + '\'s age: ');
+
+validateAge(age, legalDrinkingAge, name);
+
+var name = prompt('What is your name?: ');
+var age = prompt('What is zane '+ name + '\'s age: ');
+
+validateAge(age, legalDrinkingAge, name);
+
+
+//Function Expression
+var validateAge = function(age, legalDrinkingAge, name) {
+	if (age < legalDrinkingAge) {
+		console.log('Bs');
+	} else {
+		console.log('gs');
+	};
+};
+
+var name = prompt('What is your name?: ');
+var age = prompt('What is zane '+ name + '\'s age: ');
+
+validateAge(age, legalDrinkingAge, name);
+
+//var name1, name2, name3, name4;
+*/
+/*
+var a = 5;
+var b = 0;
+if (a = b) {
+	console.log(a);
+	console.log('true')
+} else {
+	console.log('false');
+};
+*/
+
+var names = ['waleed', 'fakher', 'massi', 'jaguar', 'boom'];
+console.log(names.length);
+console.log(names[0], names[1], names[2]);
+if (names[1] === 'waleed') {
+	console.log('hi');
+} else {
+	console.log('d');
+};
+
+var i = 1;
+while (i < 6) {
+	console.log('penis');
+	i++;
+};
+
+i = 1;
+do {
+	i++;
+	console.log('myass') 
+} while (i > 5);
+
+//while //do while // for
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
